c2f3db52f3aee90670c853d7679e4fcb6b19ea09
Sherozbek <sherozradjabov.2004@gmail.com>
2023-12-09 10:34:19 +0500

    Initial Commit

---
+++ AuthFirebase/AuthFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthFirebaseApp.swift
+//  AuthFirebase
+//
+//  Created by Sherozbek on 09/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AuthFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthFirebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AuthFirebase
+//
+//  Created by Sherozbek on 09/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
